8890f54b4d02e87c160a9ef55c3a6187b136ba54
mdsaharshital <mdsaharshitol@gmail.com>
2022-05-05 17:58:39 +0600

    db is connected env file protected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
@@ -10,8 +12,25 @@ app.use(cors());
 app.use(express.json());
 //----------------------
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zhq5r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const perfumeCollection = client.db("assignment1").collection("perfume");
+    console.log(`db is connected`);
+  } finally {
+    //
+  }
+};
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("perfume-warehouse");
+  res.send(`perfume-warehouse`);
 });
 
 app.listen(port, () => {
